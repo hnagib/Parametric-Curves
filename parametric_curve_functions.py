349f80afadefc06c621a6113c2df013908d19efb
hnagib <hnagib@alumni.ubc.ca>
2017-02-24 21:05:42 -0800

    Fix indents of function comments

--- parametric_curve_functions.py
+++ parametric_curve_functions.py
@@ -6,19 +6,19 @@ import seaborn as sns
 # Functions for constructing and plotting Bezier Curves 
 # ------------------------------------------------------------------------
 def bezier_poly_interact(ctrl_points, pt, t=0.5, P1x=-1, P1y=2, P2x=3, P2y=2):
-'''
-    Inputs
-    ctlr_points:
-        0 (or any int): Use default set of control points
-        P_i: Use set of control points specified in P_i
-    t: value between 0 and 1 for indexing a point on the blending functions 
-    pt: number of control points to use 
-    P1x: x-value of control point P1 
-    P1y: y-value of control point P1
-        
-    Outputs
-    Interactive plot of the Bezier curve and its blending functions
-'''    
+    '''
+        Inputs
+        ctlr_points:
+            0 (or any int): Use default set of control points
+            P_i: Use set of control points specified in P_i
+        t: value between 0 and 1 for indexing a point on the blending functions 
+        pt: number of control points to use 
+        P1x: x-value of control point P1 
+        P1y: y-value of control point P1
+
+        Outputs
+        Interactive plot of the Bezier curve and its blending functions
+    '''    
     if type(ctrl_points) == int:
         P_i = np.array([[-2,-1,3,4,1,0,3], [0,2,2,0,1,0.5,-0.5]])
         
@@ -71,14 +71,14 @@ def bezier_poly_interact(ctrl_points, pt, t=0.5, P1x=-1, P1y=2, P2x=3, P2y=2):
 # ------------------------------------------------------------------------
     
 def non_preiodic_knot(n,k):
-'''
-    Inputs
-    k: order of curve
-    n: n+1 control points
-    
-    Outputs
-    u_i: vector of length n+k+1 containing the knot values
-'''
+    '''
+        Inputs
+        k: order of curve
+        n: n+1 control points
+
+        Outputs
+        u_i: vector of length n+k+1 containing the knot values
+    '''
     u_i = np.zeros(n+k+1)
     for i in range(n+k+1):
         if 0<=i<k:
@@ -90,18 +90,18 @@ def non_preiodic_knot(n,k):
     return u_i
 
 def blending_func(u,u_i,k):
-'''
-    Inputs
-    u: parameter values of which the blending function is defined
-    u_i: vector of length n+k+1 containing the knot values
-    k: order of curve
-    
-    Outputs
-    N: List containing knot values and blending functions
-       N[0]: N_{i,0} contains the knot values
-       N[1]: N_{i,1} 
-       N[k]: N_{i,k} for k >= 2, contains the blending functions of degree k-1
-'''
+    '''
+        Inputs
+        u: parameter values of which the blending function is defined
+        u_i: vector of length n+k+1 containing the knot values
+        k: order of curve
+
+        Outputs
+        N: List containing knot values and blending functions
+           N[0]: N_{i,0} contains the knot values
+           N[1]: N_{i,1} 
+           N[k]: N_{i,k} for k >= 2, contains the blending functions of degree k-1
+    '''
     N =[]
     for k in range(k+1):
 
@@ -136,22 +136,22 @@ def blending_func(u,u_i,k):
 
 
 def B_spline(N,u,u_i,P_i,k):
-'''
-    Inputs
-    N: List containing knot values and blending functions
-       N[0]: N_{i,0} contains the knot values
-       N[1]: N_{i,1} 
-       N[k]: N_{i,k} for k >= 2, contains the blending functions of degree k-1
-    u: parameter values of which the blending function is defined
-    u_i: vector of length n+k+1 containing the knot values
-    P_i: array of control points
-    k: order of curve
-    
-    Outputs
-    P_u: B-Spline curve values
-        P_u[0,:]: x-components of the B-Spline curve
-        P_u[1,:]: y-components of the B-Spline curve
-''' 
+    '''
+        Inputs
+        N: List containing knot values and blending functions
+           N[0]: N_{i,0} contains the knot values
+           N[1]: N_{i,1} 
+           N[k]: N_{i,k} for k >= 2, contains the blending functions of degree k-1
+        u: parameter values of which the blending function is defined
+        u_i: vector of length n+k+1 containing the knot values
+        P_i: array of control points
+        k: order of curve
+
+        Outputs
+        P_u: B-Spline curve values
+            P_u[0,:]: x-components of the B-Spline curve
+            P_u[1,:]: y-components of the B-Spline curve
+    ''' 
     P_u = np.zeros((2,u.size))
     for i in range(len(u_i)-k):
         P_u += N[k][i]*P_i[:,i].reshape(2,1)
@@ -159,24 +159,24 @@ def B_spline(N,u,u_i,P_i,k):
     return P_u
 
 def plot_bsline_blendfunc(t, N, P_i, P_u, u, u_i,k):
-'''
-    Inputs
-    t: value between u_i[0] and u_i[-1] for indexing a point on the blending functions 
-    N: List containing knot values and blending functions
-       N[0]: N_{i,0} contains the knot values
-       N[1]: N_{i,1} 
-       N[k]: N_{i,k} for k >= 2, contains the blending functions of degree k-1
-    u: parameter values of which the blending function is defined
-    u_i: vector of length n+k+1 containing the knot values
-    P_i: array of control points
-    k: order of curve
-    P_u: B-Spline curve values
-        P_u[0,:]: x-components of the B-Spline curve
-        P_u[1,:]: y-components of the B-Spline curve
-        
-    Outputs
-    Plots the B-Spline curve and its blending functions of different degrees
-'''
+    '''
+        Inputs
+        t: value between u_i[0] and u_i[-1] for indexing a point on the blending functions 
+        N: List containing knot values and blending functions
+           N[0]: N_{i,0} contains the knot values
+           N[1]: N_{i,1} 
+           N[k]: N_{i,k} for k >= 2, contains the blending functions of degree k-1
+        u: parameter values of which the blending function is defined
+        u_i: vector of length n+k+1 containing the knot values
+        P_i: array of control points
+        k: order of curve
+        P_u: B-Spline curve values
+            P_u[0,:]: x-components of the B-Spline curve
+            P_u[1,:]: y-components of the B-Spline curve
+
+        Outputs
+        Plots the B-Spline curve and its blending functions of different degrees
+    '''
     plt.subplot(3,2,1)
     ax = plt.gca()
     plt.plot(P_u[0],P_u[1],'k')
@@ -198,20 +198,20 @@ def plot_bsline_blendfunc(t, N, P_i, P_u, u, u_i,k):
             plt.title('Blending functions of degree %d' %(k-1))
             
 def B_spline_interact(ctrl_points, pt=8, k=4, t=0.5, P1x=-1,P1y=2):
-'''
-    Inputs
-    ctlr_points:
-        0 (or any int): Use default set of control points
-        P_i: Use set of control points specified in P_i
-    t: value between 0 and 1 for indexing a point on the blending functions 
-    pt: number of control points to use 
-    k: order of curve
-    P1x: x-value of control point P1 
-    P1y: y-value of control point P1
-        
-    Outputs
-    Interactive plots of the B-Spline curve and its blending functions of different degrees
-'''
+    '''
+        Inputs
+        ctlr_points:
+            0 (or any int): Use default set of control points
+            P_i: Use set of control points specified in P_i
+        t: value between 0 and 1 for indexing a point on the blending functions 
+        pt: number of control points to use 
+        k: order of curve
+        P1x: x-value of control point P1 
+        P1y: y-value of control point P1
+
+        Outputs
+        Interactive plots of the B-Spline curve and its blending functions of different degrees
+    '''
     if type(ctrl_points) == int:
         P_i = np.array([[-2,-1,3,4,1,0,2,0], [0,2,2,0,1,0.5,-0.5,-0.5]])
 
